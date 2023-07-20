18364c05bf4d7fcc3a19479c3d61fe726486e1a7
BhagyeshPro <bhagyeshpaunikar2001@gmail.com>
2023-07-20 18:32:58 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+       <Text className="text-2xl text-black dark:text-white">App</Text>
+    </View>
+  )
+}
+
+export default App
