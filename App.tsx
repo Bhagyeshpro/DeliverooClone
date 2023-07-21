d3d0e68e568148d983869ec16bc7711208e9ccaf
BhagyeshPro <bhagyeshpaunikar2001@gmail.com>
2023-07-21 17:30:46 +0530

    feature

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const App = () => {
   return (
     <View className="bg-cyan-300 flex-1 justify-center items-center">
-       <Text className="text-2xl text-black font-bold dark:text-gray">dev</Text>
+       <Text className="text-2xl text-black font-bold dark:text-gray">features</Text>
     </View>
   )
 }
